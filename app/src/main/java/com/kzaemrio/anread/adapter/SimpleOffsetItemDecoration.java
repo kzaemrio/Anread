33c8bce05be99f519507d24dc64ff75b681d2099
X <664851432@qq.com>
2020-03-25 17:48:13 +0800

    rm header space

--- app/src/main/java/com/kzaemrio/anread/adapter/SimpleOffsetItemDecoration.java
+++ app/src/main/java/com/kzaemrio/anread/adapter/SimpleOffsetItemDecoration.java
@@ -12,9 +12,7 @@ public class SimpleOffsetItemDecoration extends RecyclerView.ItemDecoration {
         super.getItemOffsets(outRect, view, parent, state);
 
         if (parent.getAdapter() != null && parent.getAdapter().getItemCount() > 0) {
-            boolean isFirst = parent.getChildAdapterPosition(view) == 0;
             boolean isLast = parent.getChildAdapterPosition(view) == parent.getAdapter().getItemCount() - 1;
-            outRect.top = isFirst ? parent.getWidth() / 16 : 0;
             outRect.bottom = isLast ? parent.getWidth() / 8 : 0;
         }
     }
