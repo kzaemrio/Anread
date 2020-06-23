88d1f66c0ec8370a1428c4aaf67022c1a2abed08
X <664851432@qq.com>
2020-06-23 20:05:57 +0800

    add ithome filter

--- app/src/main/java/com/kzaemrio/anread/ui/ItemListViewModel.java
+++ app/src/main/java/com/kzaemrio/anread/ui/ItemListViewModel.java
@@ -77,6 +77,7 @@ public class ItemListViewModel extends AndroidViewModel {
                     }
                 })
                 .flatMap(Stream::of)
+                .filter(item -> !item.mTitle.contains("之家"))
                 .sorted(Comparator.<Item, Long>comparing(i -> i.mPubDate).reversed())
                 .map(ItemListAdapter.ViewItem::create)
                 .collect(Collectors.toList());
