a5ff819a521df722c1981b9c6cdbee6db0519e72
X <664851432@qq.com>
2020-07-06 12:45:19 +0800

    use viewBinding

--- ithome/src/main/java/com/kzaemrio/ithome/MainView.java
+++ ithome/src/main/java/com/kzaemrio/ithome/MainView.java
@@ -7,19 +7,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.FrameLayout;
-import android.widget.ProgressBar;
 
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.kzaemrio.ithome.databinding.ActivityMainBinding;
 import com.kzaemrio.ithome.event.OnHideWebViewLoadingEvent;
 import com.kzaemrio.ithome.event.OnShowWebViewLoadingEvent;
 import com.kzaemrio.ithome.event.OnWebViewHideStartEvent;
@@ -43,19 +39,12 @@ public class MainView {
 
     private final LinearLayoutManager mLayoutManager;
 
-    private final RecyclerView mRecyclerView;
-
-    private final SwipeRefreshLayout mRefreshLayout;
-
-    private final ProgressBar mProgressBar;
-    private final FrameLayout mWebViewBox;
-
-    private final FrameLayout mFrameLayout;
-
-    private WebView mWebView;
+    private final ActivityMainBinding mBinding;
 
     public MainView(Context context) {
         mContext = context;
+
+        mBinding = ActivityMainBinding.inflate(LayoutInflater.from(mContext));
 
         mAdapter = new ItemListAdapter();
         mAdapter.setOnItemClickAction(this::showDetail);
@@ -64,29 +53,12 @@ public class MainView {
         mLayoutManager = new LinearLayoutManager(mContext);
         mLayoutManager.setStackFromEnd(true);
 
-        mRecyclerView = new RecyclerView(mContext);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mRecyclerView.setAdapter(mAdapter);
+        mBinding.list.setLayoutManager(mLayoutManager);
+        mBinding.list.setAdapter(mAdapter);
 
-        mRefreshLayout = new SwipeRefreshLayout(mContext);
-        mRefreshLayout.addView(mRecyclerView);
+        mBinding.progress.setIndeterminateTintList(ColorStateList.valueOf(mContext.getColor(R.color.colorAccent)));
 
-        mProgressBar = new ProgressBar(mContext);
-        mProgressBar.setIndeterminateTintList(ColorStateList.valueOf(mContext.getColor(R.color.colorAccent)));
-        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(100, 100, Gravity.CENTER);
-
-        mWebView = new WebView(mContext.getApplicationContext());
-        mWebView.setWebViewClient(new MyWebViewClient());
-
-        mWebViewBox = new FrameLayout(mContext);
-        mWebViewBox.setBackgroundColor(Color.WHITE);
-        mWebViewBox.addView(mProgressBar, params);
-        mWebViewBox.addView(mWebView);
-        mWebViewBox.setVisibility(View.GONE);
-
-        mFrameLayout = new FrameLayout(mContext);
-        mFrameLayout.addView(mRefreshLayout);
-        mFrameLayout.addView(mWebViewBox);
+        mBinding.web.setWebViewClient(new MyWebViewClient());
     }
 
     private void shareItem(Item item) {
@@ -101,15 +73,15 @@ public class MainView {
     }
 
     public View getContentView() {
-        return mFrameLayout;
+        return mBinding.frame;
     }
 
     public void showRefresh() {
-        mRefreshLayout.setRefreshing(true);
+        mBinding.refresh.setRefreshing(true);
     }
 
     public void hideRefresh() {
-        mRefreshLayout.setRefreshing(false);
+        mBinding.refresh.setRefreshing(false);
     }
 
     public void bind(List<ItemListAdapter.ViewItem> list) {
@@ -117,11 +89,11 @@ public class MainView {
     }
 
     public void setCallback(Callback callback) {
-        mRefreshLayout.setOnRefreshListener(callback::onRefresh);
+        mBinding.refresh.setOnRefreshListener(callback::onRefresh);
     }
 
     public boolean isWebViewShow() {
-        return mWebViewBox.isShown();
+        return mBinding.box.isShown();
     }
 
     private void showDetail(Item item) {
@@ -133,7 +105,7 @@ public class MainView {
                 "<p/>" +
                 item.getDes();
 
-        mWebView.loadDataWithBaseURL(
+        mBinding.web.loadDataWithBaseURL(
                 "file:///android_asset/",
                 html,
                 "text/html",
@@ -141,12 +113,12 @@ public class MainView {
                 null
         );
 
-        ObjectAnimator animator = ObjectAnimator.ofFloat(mWebViewBox, View.TRANSLATION_Y, mFrameLayout.getHeight(), 0);
+        ObjectAnimator animator = ObjectAnimator.ofFloat(mBinding.box, View.TRANSLATION_Y, mBinding.frame.getHeight(), 0);
         animator.addListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationStart(Animator animation) {
                 super.onAnimationStart(animation);
-                mWebViewBox.setVisibility(View.VISIBLE);
+                mBinding.box.setVisibility(View.VISIBLE);
             }
 
             @Override
@@ -160,7 +132,7 @@ public class MainView {
     }
 
     public void hideWebView() {
-        ObjectAnimator animator = ObjectAnimator.ofFloat(mWebViewBox, View.TRANSLATION_Y, 0, mFrameLayout.getHeight());
+        ObjectAnimator animator = ObjectAnimator.ofFloat(mBinding.box, View.TRANSLATION_Y, 0, mBinding.frame.getHeight());
         animator.addListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationStart(Animator animation) {
@@ -171,7 +143,7 @@ public class MainView {
             @Override
             public void onAnimationEnd(Animator animation) {
                 super.onAnimationEnd(animation);
-                mWebViewBox.setVisibility(View.GONE);
+                mBinding.box.setVisibility(View.GONE);
             }
         });
         animator.setDuration(duration);
@@ -180,38 +152,35 @@ public class MainView {
 
     public void destroyWebView() {
         // Make sure you remove the WebView from its parent view before doing anything.
-        mWebViewBox.removeAllViews();
+        mBinding.box.removeAllViews();
 
-        mWebView.clearHistory();
+        mBinding.web.clearHistory();
 
         // NOTE: clears RAM cache, if you pass true, it will also clear the disk cache.
         // Probably not a great idea to pass true if you have other WebViews still alive.
-        mWebView.clearCache(true);
+        mBinding.web.clearCache(true);
 
         // Loading a blank page is optional, but will ensure that the WebView isn't doing anything when you destroy it.
-        mWebView.loadUrl("about:blank");
+        mBinding.web.loadUrl("about:blank");
 
-        mWebView.onPause();
-        mWebView.removeAllViews();
+        mBinding.web.onPause();
+        mBinding.web.removeAllViews();
 
         // NOTE: This pauses JavaScript execution for ALL WebViews,
         // do not use if you have other WebViews still alive.
         // If you create another WebView after calling this,
         // make sure to call mWebView.resumeTimers().
-        mWebView.pauseTimers();
-
-        // Null out the reference so that you don't end up re-using it.
-        mWebView = null;
+        mBinding.web.pauseTimers();
     }
 
     public long getPubDate() {
-        View childAt = mRecyclerView.getChildAt(0);
-        int position = mRecyclerView.getChildAdapterPosition(childAt);
+        View childAt = mBinding.list.getChildAt(0);
+        int position = mBinding.list.getChildAdapterPosition(childAt);
         return mAdapter.getCurrentList().get(position).getItem().getPubDate();
     }
 
     public int getOffset() {
-        return mRecyclerView.getChildAt(0).getTop();
+        return mBinding.list.getChildAt(0).getTop();
     }
 
     public boolean isInit() {
@@ -223,23 +192,23 @@ public class MainView {
     }
 
     public void alertHasNew() {
-        mRecyclerView.postDelayed(() -> mRecyclerView.smoothScrollBy(0, -50), 300);
+        mBinding.list.postDelayed(() -> mBinding.list.smoothScrollBy(0, -50), 300);
     }
 
     public void showWebViewLoading() {
-        mProgressBar.setVisibility(View.VISIBLE);
+        mBinding.progress.setVisibility(View.VISIBLE);
     }
 
     public void hideWebViewLoading() {
-        mProgressBar.setVisibility(View.GONE);
+        mBinding.progress.setVisibility(View.GONE);
     }
 
     public void hideList() {
-        mRefreshLayout.setVisibility(View.GONE);
+        mBinding.refresh.setVisibility(View.GONE);
     }
 
     public void showList() {
-        mRefreshLayout.setVisibility(View.VISIBLE);
+        mBinding.refresh.setVisibility(View.VISIBLE);
     }
 
     public interface Callback {
